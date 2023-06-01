2b078b78810f7a0434c8a684b38675b7a67477ed
Victor <barosv@yandex.ru>
2023-06-01 12:38:00 +0400

    на проверку

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,8 @@ gulp.task('watch', function() {
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
     gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+    gulp.watch("src/video/**/*").on('all', gulp.parallel('video'));
+
 });
 
 gulp.task('html', function () {
@@ -68,4 +70,11 @@ gulp.task('images', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('video', function () {
+    return gulp.src("src/video/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/video"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images', 'video'));
